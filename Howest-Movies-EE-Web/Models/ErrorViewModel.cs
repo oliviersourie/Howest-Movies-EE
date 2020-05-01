b7d4e751e60e10208bbf8facd91884c808cd5800
Olivier Sourie <Olivier.Sourie@howest.be>
2020-05-01 16:31:39 +0200

    Add project files.

---
+++ Howest-Movies-EE-Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Howest_Movies_EE_Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
